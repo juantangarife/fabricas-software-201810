afabe9feece14160109e7b223ee6faec0ccc639b
d-soto11 <d.soto11@uniandes.edu.co>
2018-05-22 08:53:47 -0500

    Clean refactor

--- producto/src/main/java/comrueda/producto/ejb/ProductoLogic.java
+++ producto/src/main/java/comrueda/producto/ejb/ProductoLogic.java
@@ -1,22 +1,64 @@
 package comrueda.producto.ejb;
 
 import comrueda.producto.entities.ProductoEntity;
+import comrueda.producto.persistance.ProductoPersistance;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 //import javax.ejb.Remote;
 
 //@Remote(ProductoLogic.class)
-public interface ProductoLogic {
-    public ProductoEntity crearProducto(ProductoEntity entity);
+//public interface ProductoLogic {
+//    public ProductoEntity crearProducto(ProductoEntity entity);
+//
+//    public List<ProductoEntity> listarProductos();
+//
+//    public ProductoEntity buscar(Long id);
+//
+//    public ProductoEntity actualizar(ProductoEntity entity);
+//
+//    public void borrar(ProductoEntity entity);
+//
+//}
 
-    public List<ProductoEntity> listarProductos();
+@Stateless(name="ProductoLogic")
+public class ProductoLogic {
 
-    public ProductoEntity buscar(Long id);
+    private static final Logger LOGGER = Logger.getLogger(ProductoLogic.class.getName());
 
-    public ProductoEntity actualizar(ProductoEntity entity);
+    @Inject
+    private ProductoPersistance persistence;
 
-    public void borrar(ProductoEntity entity);
+    public ProductoEntity crearProducto(ProductoEntity entity) {
+        LOGGER.info("Inicia proceso de creación de producto");
+        persistence.crear(entity);
+        LOGGER.info("Termina proceso de creación de producto");
+        return entity;
+    }
+
+    public List<ProductoEntity> listarProductos() {
+        LOGGER.info("Inicia proceso de consultar todos los productos");
+        List<ProductoEntity> productos = persistence.listarProductos();
+        LOGGER.info("Termina proceso de consultar todos los productos");
+        return productos;
+    }
+
+    public ProductoEntity buscar(Long id) {
+        return persistence.buscar(id);
+    }
+
+    public ProductoEntity actualizar(ProductoEntity entity) {
+        return persistence.actualizar(entity);
+    }
+
+
+    public void borrar(ProductoEntity entity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar producto con id={0}", entity.getId());
+        persistence.borrar(entity);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar productoo con id={0}", entity.getId());
+    }
 
 }
-
-
--- producto/src/main/java/comrueda/producto/persistance/ProductoPersistance.java
+++ producto/src/main/java/comrueda/producto/persistance/ProductoPersistance.java
@@ -3,18 +3,62 @@ package comrueda.producto.persistance;
 import comrueda.producto.entities.ProductoEntity;
 
 import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 //import javax.ejb.Remote;
 
-public interface ProductoPersistance {
+//public interface ProductoPersistance {
+//
+//    public ProductoEntity crear(ProductoEntity entity);
+//
+//    public List<ProductoEntity> listarProductos();
+//
+//    public ProductoEntity buscar(Long id);
+//
+//    public ProductoEntity actualizar(ProductoEntity entity);
+//
+//    public void borrar(ProductoEntity entity);
+//
+//}
 
-    public ProductoEntity crear(ProductoEntity entity);
+@Stateless(name="ProductoPersistance")
+public class ProductoPersistance {
 
-    public List<ProductoEntity> listarProductos();
+    private static final Logger LOGGER = Logger.getLogger(ProductoPersistance.class.getName());
 
-    public ProductoEntity buscar(Long id);
+    @PersistenceContext(unitName = "ComruedaPU")
+    protected EntityManager em;
 
-    public ProductoEntity actualizar(ProductoEntity entity);
+    public ProductoEntity crear(ProductoEntity entity) {
+        LOGGER.info("Creando un producto nuevo");
+        em.persist(entity);
+        LOGGER.info("Nuevo producto creado");
+        return entity;
+    }
 
-    public void borrar(ProductoEntity entity);
+    public List<ProductoEntity> listarProductos() {
+        LOGGER.info("Consultando todas los productos");
+        TypedQuery<ProductoEntity> query = em.createQuery("select u from ProductoEntity u", ProductoEntity.class);
+        return query.getResultList();
+    }
+
+    public ProductoEntity buscar(Long id) {
+        return em.find(ProductoEntity.class, id);
+    }
+
+    public ProductoEntity actualizar(ProductoEntity entity) {
+        return em.merge(entity);
+    }
+
+    public void borrar(ProductoEntity entity) {
+        if (!em.contains(entity)) {
+            entity = em.merge(entity);
+        }
+        em.remove(entity);
+    }
 
 }
